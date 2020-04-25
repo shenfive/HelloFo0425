c3583f00c80846224f2d4c660ad66021e1b494bf
Danny Shen <shenfive@gmail.com>
2020-04-25 14:25:35 +0800

    page 2 Table

--- HelloFo0425/PageTwoViewController.swift
+++ HelloFo0425/PageTwoViewController.swift
@@ -8,25 +8,31 @@
 
 import UIKit
 
-class PageTwoViewController: UIViewController {
+class PageTwoViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+
 
     var nickName = ""
     
+    @IBOutlet weak var foTableView: UITableView!
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.title = "討論區清單" 
+        super.viewDidLoad()        
+        self.title = "討論區清單"
+        foTableView.delegate = self
+        foTableView.dataSource = self
+    }
+   
+    
+    
+    //MARK:TableView Delegate & DataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "line:\(indexPath.row)"
+        return cell
     }
-    */
+    
 
 }
